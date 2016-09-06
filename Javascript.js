f016818bd33280e86c863ac20bdb8d581aabd1ba
JapTePrind <stefan_stafie@yahoo.com>
2016-09-06 22:07:40 +0300

    start

---
+++ Javascript.js
@@ -0,0 +1,17 @@
+var myName = "Whac-A-Mole";
+
+var red = [0, 100, 63];
+var orange = [40, 100, 60];
+var green = [75, 100, 40];
+var white
+var black
+var letterColors = [orange, white];
+ 
+drawName(myName, letterColors);
+
+bubbleShape = 'square';
+bounceBubbles();
+
+$(".s").fadeIn(2000);
+
+$(".myImage").show(12000);
